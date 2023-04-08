794962d5a36c0375e211ce2a81b45749476cef6b
bhushan8554 <bhushantaru4297@gmail.com>
2023-04-08 15:16:42 +0530

    performed cleaning

--- src/main/java/com/adobe/model/PostDTO.java
+++ src/main/java/com/adobe/model/PostDTO.java
@@ -2,10 +2,8 @@ package com.adobe.model;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- src/main/java/com/adobe/model/UserDTO.java
+++ src/main/java/com/adobe/model/UserDTO.java
@@ -1,12 +1,9 @@
 package com.adobe.model;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
