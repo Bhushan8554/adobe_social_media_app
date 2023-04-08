ee2e51ccbd39f3d65d8874665e3bb4fbce2ffcc2
bhushan8554 <bhushantaru4297@gmail.com>
2023-04-08 14:36:23 +0530

    added mapping in user

--- src/main/java/com/adobe/model/User.java
+++ src/main/java/com/adobe/model/User.java
@@ -1,9 +1,12 @@
 package com.adobe.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,6 +24,8 @@ public class User {
 	private String bio;
 	private LocalDateTime created_at;
 	private LocalDateTime updated_at;
+	@OneToMany
+	private List<Post> post=new ArrayList<>();
 	
 	
 	
