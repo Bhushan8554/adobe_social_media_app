354598e453e21ae91596533d6e9df60077a23aa8
bhushan8554 <bhushantaru4297@gmail.com>
2023-04-09 23:27:34 +0530

    added view delete add edit

--- src/main/resources/static/scripts/index.js
+++ src/main/resources/static/scripts/index.js
@@ -4,196 +4,397 @@
 
 //data fetch
 function fetchData(){
-fetch("http://localhost:8080/users")
-  .then(response => response.json())
-  .then(result => append(result))
-  .catch(error => console.log('error', error));
-
-}
-function fetchDataPost(){
-    fetch("http://localhost:8080/posts")
+    fetch("http://localhost:8080/users")
       .then(response => response.json())
-      .then(result => appendPost(result))
+      .then(result => append(result))
       .catch(error => console.log('error', error));
     
-}
-
-let appendPost=(arr)=>{
-    
-    // console.log(arr); 
-    let cont=document.querySelector("#con");
-    cont.innerHTML=null;
-     for(let i=0;i<arr.length;i++){
-        let div=document.createElement("div");
-        div.className="container";
-        div.innerHTML=`<div class="row">
-        <div class="col">
-            <p>${arr[i].content}</p>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            <p>likes :- ${arr[i].likes}</p>
-        </div>
-        <div class="col">
-            <button class="btn btn-sm btn-primary" onclick="likePost()"> like post</button>
-        </div>
-        <div class="col">
-            <button class="btn btn-sm" onclick="dislikePost()"> dislike post</button>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            <p>created at :- ${arr[i].created_at}</p>
-        </div>
-        <div class="col">
-            <p>updated at :- ${arr[i].updated_at}</p>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            <button class="btn btn-primary btn-sm" onclick="viewPost()">View</button>
-        </div>
-        <div class="col">
-            <button class="btn btn-secondary btn-sm" onclick="editPost()">Edit</button>
-        </div>
-        <div class="col">
-            <button class="btn btn-danger btn-sm" onclick="deletePost()">Delete</button>
-        </div>
-    </div>`;
-
-    cont.append(div);
+    }
+    function fetchDataPost(){
+        fetch("http://localhost:8080/posts")
+          .then(response => response.json())
+          .then(result => appendPost(result))
+          .catch(error => console.log('error', error));
         
-     }
-
-
- }
-
-
-function showAddPostForm(){
-    let cont=document.querySelector("#con");
-    cont.innerHTML=null;
-    cont.innerHTML=`<form onsubmit="addPostInData()">
-    <h3>Add Post</h3>
-    <div class="form-group">
-      <label for="exampleInputEmail1">User Id</label>
-      <input type="number" class="form-control" id="id" aria-describedby="emailHelp" placeholder="Enter user id">
-    </div>
-    <div class="form-group">
-      <label for="bio">Content</label>
-      <textarea class="form-control" id="content" rows="3" placeholder="content"></textarea>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form>`;
-}
-
-function addPostInData(){
-    event.preventDefault();
-    let id=document.querySelector("#id").value;
-    let content=document.querySelector("#content").value;
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    var raw = JSON.stringify({
-    "content": content
-    });
-
-    var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: raw,
-    redirect: 'follow'
-    };
-
-    fetch(`http://localhost:8080/posts?u_id=${id}`,requestOptions)
-    .then(response => response.json())
-    .then(result => alert(JSON.stringify(result)))
-    .catch(error => alert('error', error));
-
-
-}
-
-function addUserInData(){
-    event.preventDefault();
-    let id=document.querySelector("#id").value;
-    let name=document.querySelector("#name").value;
-    let email=document.querySelector("#email").value;
-    let bio=document.querySelector("#bio").value;
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    var raw = JSON.stringify({
-    "id": id,
-    "name": name,
-    "email": email,
-    "bio": bio
-    });
-
-    var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: raw,
-    redirect: 'follow'
-    };
-
-    fetch("http://localhost:8080/users",requestOptions)
-    .then(response => response.json())
-    .then(result => alert(JSON.stringify(result)))
-    .catch(error => alert('error', error));
-
-
-}
-
- 
- let append=(arr)=>{
+    }
+    function likePost(id){
+        postMapp(`http://localhost:8080/posts/${id}/like`,"POST")
+        fetchDataPost();
     
-    // console.log(arr); 
-    let cont=document.querySelector("#con");
-    cont.innerHTML=null;
-     for(let i=0;i<arr.length;i++){
+    }
+    function dislikePost(id){
+        postMapp(`http://localhost:8080/posts/${id}/unlike`,"POST")
+        fetchDataPost();
+    }
+    
+    function deleteUser(id){
+        postMapp(`http://localhost:8080/users/${id}`,"DELETE")
+        fetchData();
+    }
+    function deletePost(id){
+        postMapp(`http://localhost:8080/posts/${id}`,"DELETE")
+        fetchDataPost();
+    }
+
+    function viewPost(id){
+        fetch(`http://localhost:8080/posts/${id}`)
+        .then(response => response.json())
+        .then(result => viewPost2(result))
+        .catch(error => console.log('error', error));
+    }
+    function viewPost2(data){
+        let cont=document.querySelector("#con");
+        cont.innerHTML=null;
         let div=document.createElement("div");
-        div.className="container";
-        div.innerHTML=`<div class="row">
-        <div class="col">
-            <h1>${arr[i].name}</h1>
+            div.className="container";
+            div.innerHTML=`<div class="row">
+            <div class="col">
+                <p>${data.content}</p>
+            </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            <p>${arr[i].email}</p>
+        <div class="row">
+            <div class="col">
+                <p>likes :- ${data.likes}</p>
+            </div>
+            <div class="col">
+                <button class="btn btn-sm btn-primary" onclick="likePost(${data.id})"> like post</button>
+            </div>
+            <div class="col">
+                <button class="btn btn-sm" onclick="dislikePost(${data.id})"> dislike post</button>
+            </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            <p>${arr[i].bio}</p>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            <p>created at :- ${arr[i].created_at}</p>
-        </div>
-        <div class="col">
-            <p>updated at :- ${arr[i].updated_at}</p>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            <button class="btn btn-primary btn-sm" onclick="viewUser()">View</button>
-        </div>
-        <div class="col">
-            <button class="btn btn-secondary btn-sm" onclick="editUser()">Edit</button>
-        </div>
-        <div class="col">
-            <button class="btn btn-danger btn-sm" onclick="deleteUser()">Delete</button>
-        </div>
-    </div>`;
+        <div class="row">
+            <div class="col">
+                <p>created at :- ${data.created_at}</p>
+            </div>
+            <div class="col">
+                <p>updated at :- ${data.updated_at}</p>
+            </div>
+        </div>`;
+    
+        cont.append(div);
+            
+    }
+    function viewUser(id){
+        fetch(`http://localhost:8080/users/${id}`)
+      .then(response => response.json())
+      .then(result => viewUser2(result))
+      .catch(error => console.log('error', error));
+    
+    }
 
-    cont.append(div);
+    function viewUser2(data){
+        //let d=postMapp(`http://localhost:8080/users/${id}`,"GET")
+        console.log(data);
+        let cont=document.querySelector("#con");
+        cont.innerHTML=null;
+        let div=document.createElement("div");
+            div.className="container";
+            div.innerHTML=`<div class="row">
+            <div class="col">
+                <h1>${data.name}</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <p>${data.email}</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <p>${data.bio}</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <p>created at :- ${data.created_at}</p>
+            </div>
+            <div class="col">
+                <p>updated at :- ${data.updated_at}</p>
+            </div>
+        </div>`;
+    
+        cont.append(div);
+            
+    }
+
+    function editPost(id){
+        let data=postMapp(`http://localhost:8080/posts/${id}`,"GET");
+        let cont=document.querySelector("#con");
+        cont.innerHTML=null;
+        cont.innerHTML=`<form onsubmit="updatePost(${id})">
+        <h3>update Post Content</h3>
+        <div class="form-group">
+          <label for="bio">Content</label>
+          <textarea class="form-control" id="content" rows="3" placeholder="content"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>`;
+    }
+    function updatePost(id){
+        event.preventDefault();
+        let content=document.querySelector("#content").value;
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        var raw = JSON.stringify({
+        "content": content
+        });
+    
+        var requestOptions = {
+        method: 'PUT',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+        };
+    
+        fetch(`http://localhost:8080//posts/${id}`,requestOptions)
+        .then(response => response.json())
+        .then(result => alert(JSON.stringify(result)))
+        .catch(error => alert('error', error));
+    
+    }
+
+    function editUser(id){
+        let data=postMapp(`http://localhost:8080/users/${id}`,"GET");
+        let cont=document.querySelector("#con");
+        cont.innerHTML=null;
+        cont.innerHTML=`<form onsubmit="updateUser(${id})">
+        <h3>update User</h3>
+        <div class="form-group">
+          <label for="name">Name</label>
+          <input type="text" class="form-control" id="name" aria-describedby="emailHelp" placeholder="Enter name">
+        </div>
+        <div class="form-group">
+          <label for="bio">Bio</label>
+          <input type="text" class="form-control" id="bio" placeholder="Bio">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>`;
+    }
+    function updateUser(id){
+        event.preventDefault();
+        let name=document.querySelector("#name").value;
+        let bio=document.querySelector("#bio").value;
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        var raw = JSON.stringify({
+        "name": name,
+        "bio": bio
+        });
+    
+        var requestOptions = {
+        method: 'PUT',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+        };
+    
+        fetch(`http://localhost:8080/users/${id}`,requestOptions)
+        .then(response => response.json())
+        .then(result => alert(JSON.stringify(result)))
+        .catch(error => alert('error', error));
+    
+    
+    }
+
+    let appendPost=(arr)=>{
         
+        // console.log(arr); 
+        let cont=document.querySelector("#con");
+        cont.innerHTML=null;
+         for(let i=0;i<arr.length;i++){
+            let div=document.createElement("div");
+            div.className="container";
+            div.innerHTML=`<div class="row">
+            <div class="col">
+                <p>${arr[i].content}</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <p>likes :- ${arr[i].likes}</p>
+            </div>
+            <div class="col">
+                <button class="btn btn-sm btn-primary" onclick="likePost(${arr[i].id})"> like post</button>
+            </div>
+            <div class="col">
+                <button class="btn btn-sm" onclick="dislikePost(${arr[i].id})"> dislike post</button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <p>created at :- ${arr[i].created_at}</p>
+            </div>
+            <div class="col">
+                <p>updated at :- ${arr[i].updated_at}</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <button class="btn btn-primary btn-sm" onclick="viewPost(${arr[i].id})">View</button>
+            </div>
+            <div class="col">
+                <button class="btn btn-secondary btn-sm" onclick="editPost(${arr[i].id})">Edit</button>
+            </div>
+            <div class="col">
+                <button class="btn btn-danger btn-sm" onclick="deletePost(${arr[i].id})">Delete</button>
+            </div>
+        </div>`;
+    
+        cont.append(div);
+            
+         }
+    
+    
      }
-
-
- }
-
-
-
-
-
+    
+    
+    function showAddPostForm(){
+        let cont=document.querySelector("#con");
+        cont.innerHTML=null;
+        cont.innerHTML=`<form onsubmit="addPostInData()">
+        <h3>Add Post</h3>
+        <div class="form-group">
+          <label for="exampleInputEmail1">User Id</label>
+          <input type="number" class="form-control" id="id" aria-describedby="emailHelp" placeholder="Enter user id">
+        </div>
+        <div class="form-group">
+          <label for="bio">Content</label>
+          <textarea class="form-control" id="content" rows="3" placeholder="content"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>`;
+    }
+    
+    function addPostInData(){
+        event.preventDefault();
+        let id=document.querySelector("#id").value;
+        let content=document.querySelector("#content").value;
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        var raw = JSON.stringify({
+        "content": content
+        });
+    
+        var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+        };
+    
+        fetch(`http://localhost:8080/posts?u_id=${id}`,requestOptions)
+        .then(response => response.json())
+        .then(result => alert(JSON.stringify(result)))
+        .catch(error => alert('error', error));
+    
+    
+    }
+    
+    function addUserInData(){
+        event.preventDefault();
+        let id=document.querySelector("#id").value;
+        let name=document.querySelector("#name").value;
+        let email=document.querySelector("#email").value;
+        let bio=document.querySelector("#bio").value;
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        var raw = JSON.stringify({
+        "id": id,
+        "name": name,
+        "email": email,
+        "bio": bio
+        });
+    
+        var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+        };
+    
+        fetch("http://localhost:8080/users",requestOptions)
+        .then(response => response.json())
+        .then(result => alert(JSON.stringify(result)))
+        .catch(error => alert('error', error));
+    
+    
+    }
+    
+    function postMapp(url,method){
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        var raw = JSON.stringify({
+        
+        });
+    
+        var requestOptions = {
+        method: method,
+        headers: myHeaders,
+        redirect: 'follow'
+        };
+        let data;
+        fetch(url,requestOptions)
+        .then(response => response.json())
+        .then(result => data=result)
+        .catch(error => alert('error', error));
+        return data;
+    }
+    
+     
+     let append=(arr)=>{
+        
+        // console.log(arr); 
+        let cont=document.querySelector("#con");
+        cont.innerHTML=null;
+         for(let i=0;i<arr.length;i++){
+            let div=document.createElement("div");
+            div.className="container";
+            div.innerHTML=`<div class="row">
+            <div class="col">
+                <h1>${arr[i].name}</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <p>${arr[i].email}</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <p>${arr[i].bio}</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <p>created at :- ${arr[i].created_at}</p>
+            </div>
+            <div class="col">
+                <p>updated at :- ${arr[i].updated_at}</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <button class="btn btn-primary btn-sm" onclick="viewUser(${arr[i].id})">View</button>
+            </div>
+            <div class="col">
+                <button class="btn btn-secondary btn-sm" onclick="editUser(${arr[i].id})">Edit</button>
+            </div>
+            <div class="col">
+                <button class="btn btn-danger btn-sm" onclick="deleteUser(${arr[i].id})">Delete</button>
+            </div>
+        </div>`;
+    
+        cont.append(div);
+            
+         }
+    
+    
+     }
+    
+    
+    
+    
+    
+    
+    
