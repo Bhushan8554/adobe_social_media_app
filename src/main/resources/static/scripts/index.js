6652393e3ca629227bbb7846c599bc171ff26a66
bhushan8554 <bhushantaru4297@gmail.com>
2023-04-09 20:36:31 +0530

    finaly list user working and added

--- src/main/resources/static/scripts/index.js
+++ src/main/resources/static/scripts/index.js
@@ -30,23 +30,72 @@ fetch("http://localhost:8080/users")
  
  let append=(arr)=>{
     
-     console.log(arr); 
+    // console.log(arr); 
+    let cont=document.querySelector("#con");
      for(let i=0;i<arr.length;i++){
         let div=document.createElement("div");
-        let name=document.createElement("h3");
-        name.innerText=arr[i].name;
-        let bio=document.createElement("p");
-        bio.innerText=arr[i].bio;
-        let email=document.createElement("p");
-        email.innerText=arr[i].email;
-        let up=document.createElement("p");
-        up.innerText=arr[i].updated_at?arr[i].updated_at:arr[i].created_at;
+        div.className="container";
+        div.innerHTML=`<div class="row">
+        <div class="col">
+            <h1>${arr[i].name}</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <p>${arr[i].email}</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <p>${arr[i].bio}</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <p>created at :- ${arr[i].created_at}</p>
+        </div>
+        <div class="col">
+            <p>updated at :- ${arr[i].updated_at}</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <button class="btn btn-primary btn-sm" onclick="viewUser()">View</button>
+        </div>
+        <div class="col">
+            <button class="btn btn-secondary btn-sm" onclick="editUser()">Edit</button>
+        </div>
+        <div class="col">
+            <button class="btn btn-danger btn-sm" onclick="deleteUser()">Delete</button>
+        </div>
+    </div>`;
 
-        div.append(name,email,bio,up);
-        document.getElementById("container").append(div);
+    cont.append(div);
+        // let name=document.createElement("h3");
+        // name.innerText=arr[i].name;
+        // let bio=document.createElement("p");
+        // bio.innerText=arr[i].bio;
+        // let email=document.createElement("p");
+        // email.innerText=arr[i].email;
+        // let up=document.createElement("p");
+        // up.innerText=arr[i].updated_at?arr[i].updated_at:arr[i].created_at;
+        // let btn=document.createElement("button");
+        // btn.innerText="View";
+        // let btn2=document.createElement("button");
+        // btn2.innerText="Edit";
+        // let btn3=document.createElement("button");
+        // btn3.innerText="Delete";
+
+        // div.append(name,email,bio,up,btn,btn2,btn3);
+        // document.getElementById("container").append(div);
+
+
      }
 
-//    console.log(data)
+
  }
+
+
+
 
 
