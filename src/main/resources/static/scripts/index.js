f2fbcde1b5299d690aa64218e365e86e53d5a94d
bhushan8554 <bhushantaru4297@gmail.com>
2023-04-09 13:15:23 +0530

    index.html work

--- src/main/resources/static/scripts/index.js
+++ src/main/resources/static/scripts/index.js
@@ -1,5 +1,10 @@
+// Navbar
+
+
+
+//data fetch
 function fetchData(){
-   var myHeaders = new Headers();
+    var myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
 
 var raw = JSON.stringify({
@@ -16,13 +21,32 @@ var requestOptions = {
   redirect: 'follow'
 };
 
-fetch("/users")
-  .then(response => response.text())
-  .then(result => console.log(result))
+fetch("http://localhost:8080/users")
+  .then(response => response.json())
+  .then(result => append(result))
   .catch(error => console.log('error', error));
-}
 
-let append=(data)=>{
-    console.log("4");
-  console.log(data)
-};
+}
+ 
+ let append=(arr)=>{
+    
+     console.log(arr); 
+     for(let i=0;i<arr.length;i++){
+        let div=document.createElement("div");
+        let name=document.createElement("h3");
+        name.innerText=arr[i].name;
+        let bio=document.createElement("p");
+        bio.innerText=arr[i].bio;
+        let email=document.createElement("p");
+        email.innerText=arr[i].email;
+        let up=document.createElement("p");
+        up.innerText=arr[i].updated_at?arr[i].updated_at:arr[i].created_at;
+
+        div.append(name,email,bio,up);
+        document.getElementById("container").append(div);
+     }
+
+//    console.log(data)
+ }
+
+
